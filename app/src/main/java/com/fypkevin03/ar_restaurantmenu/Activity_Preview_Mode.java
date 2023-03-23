abf86c95fe05591047d2cb45411e9f9f9e9e2036
FinnTheHuman411 <lbenson997@gmail.com>
2023-03-23 23:24:06 +0800

    tweakedAR

--- app/src/main/java/com/fypkevin03/ar_restaurantmenu/Activity_Preview_Mode.java
+++ app/src/main/java/com/fypkevin03/ar_restaurantmenu/Activity_Preview_Mode.java
@@ -1,27 +1,132 @@
 package com.fypkevin03.ar_restaurantmenu;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.SceneView;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
-public class Activity_Preview_Mode extends AppCompatActivity {
+import java.lang.ref.WeakReference;
 
-    ArFragment arFragment;
+public class Activity_Preview_Mode extends AppCompatActivity implements
+        FragmentOnAttachListener,
+        BaseArFragment.OnTapArPlaneListener,
+        BaseArFragment.OnSessionConfigurationListener,
+        ArFragment.OnViewCreatedListener {
+
+    private ArFragment arFragment;
+    private Renderable model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_mode);
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
         final String food_model = getIntent().getStringExtra("product_model");
 
         // Load model.glb from assets folder or http url
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
-        arFragment.setOnTapPlaneGlbModel(food_model);
+//        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+//        arFragment.setOnTapPlaneGlbModel(food_model);
 //        arFragment.setOnTapPlaneGlbModel("https://github.com/codemaker2015/3d-models/blob/master/glb_files/model.glb?raw=true");
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+        }
+
+        loadModels(food_model);
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnSessionConfigurationListener(this);
+            arFragment.setOnViewCreatedListener(this);
+            arFragment.setOnTapArPlaneListener(this);
+        }
+    }
+
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+        if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+            config.setDepthMode(Config.DepthMode.AUTOMATIC);
+        }
+    }
+
+    @Override
+    public void onViewCreated(ArSceneView arSceneView) {
+        arFragment.setOnViewCreatedListener(null);
+
+        // Fine adjust the maximum frame rate
+        arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL);
+    }
+
+    public void loadModels(String food_model) {
+        WeakReference<Activity_Preview_Mode> weakActivity = new WeakReference<>(this);
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse(food_model))
+                .setIsFilamentGltf(true)
+                .setAsyncLoadEnabled(true)
+                .build()
+                .thenAccept(model -> {
+                    Activity_Preview_Mode activity = weakActivity.get();
+                    if (activity != null) {
+                        activity.model = model;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(
+                            this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+    }
+
+    @Override
+    public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+        if (model == null) {
+            Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create the Anchor.
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        // Create the transformable model and add it to the anchor.
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+        model.setParent(anchorNode);
+        model.setRenderable(this.model)
+                .animate(true).start();
+        model.getScaleController().setEnabled(false);
+        model.getRenderableInstance().setCulling(false);
+        model.select();
+
     }
 }
