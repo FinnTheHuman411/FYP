109c22edb64c01791e2fef1d69ef6ea9e1ed3d32
FinnTheHuman411 <lbenson997@gmail.com>
2023-03-25 13:23:37 +0800

    updated

--- app/src/main/java/com/fypkevin03/ar_restaurantmenu/Adapter_Cart.java
+++ app/src/main/java/com/fypkevin03/ar_restaurantmenu/Adapter_Cart.java
@@ -1,6 +1,7 @@
 package com.fypkevin03.ar_restaurantmenu;
 
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
@@ -19,6 +20,8 @@ import static android.database.sqlite.SQLiteDatabase.openOrCreateDatabase;
 public class Adapter_Cart extends ArrayAdapter<Object_Cart> {
 
     public Context mContext;
+    double price;
+    int count;
 
     public Adapter_Cart(@NonNull Context context, ArrayList<Object_Cart> cartObjects) {
         super(context, 0, cartObjects);
@@ -46,14 +49,21 @@ public class Adapter_Cart extends ArrayAdapter<Object_Cart> {
         // Populate the data into the template view using the data object
         food_product_image.setImageResource(obj.image);
         food_product_name.setText(obj.product_name);
-        food_price.setText("Price: $" + obj.price);
-        food_count.setText("Count: " + obj.count);
+        price = obj.price * obj.count;
+        count = obj.count;
+        food_price.setText("Price: $" + price);
+        food_count.setText("Count: " + count);
+
 
         //button
         com.google.android.material.button.MaterialButton btn_remove = (com.google.android.material.button.MaterialButton) convertView.findViewById(R.id.btn_remove);
+        com.google.android.material.button.MaterialButton btn_add = (com.google.android.material.button.MaterialButton) convertView.findViewById(R.id.btn_add);
+        com.google.android.material.button.MaterialButton btn_minus = (com.google.android.material.button.MaterialButton) convertView.findViewById(R.id.btn_minus);
 
         // Cache row position inside the button using `setTag`
         btn_remove.setTag(position);
+        btn_add.setTag(position);
+        btn_minus.setTag(position);
 
         // Attach the click event handler
         btn_remove.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +77,48 @@ public class Adapter_Cart extends ArrayAdapter<Object_Cart> {
 
                 SQLiteDatabase cart = mContext.openOrCreateDatabase("cart",mContext.MODE_PRIVATE,null);
                 cart.delete("cart", "_id = " + object.cartID, null);
+            }
+        });
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                int position = (Integer) view.getTag();
+                // Access the row position here to get the correct data item
+                Object_Cart object = getItem(position);
+                count = count+1;
+                price = obj.price * count;
+                food_price.setText("Price: $" + price);
+                food_count.setText("Count: " + count);
+
+                SQLiteDatabase cart = mContext.openOrCreateDatabase("cart",mContext.MODE_PRIVATE,null);
+                ContentValues cv = new ContentValues();
+                cv.put("price", price);
+                cv.put("count", count);
+                cart.update("cart", cv, "_id = ?", new String [] {Integer.toString(object.cartID)});
+            }
+        });
+
+        btn_minus.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                int position = (Integer) view.getTag();
+                // Access the row position here to get the correct data item
+                Object_Cart object = getItem(position);
+                if (count > 1) {
+                    count = count-1;
+                    price = obj.price * count;
+                    food_price.setText("Price: $" + price);
+                    food_count.setText("Count: " + count);
+
+                    SQLiteDatabase cart = mContext.openOrCreateDatabase("cart",mContext.MODE_PRIVATE,null);
+                    ContentValues cv = new ContentValues();
+                    cv.put("price", price);
+                    cv.put("count", count);
+                    cart.update("cart", cv, "_id = ?", new String [] {Integer.toString(object.cartID)});
+                }
             }
         });
 
